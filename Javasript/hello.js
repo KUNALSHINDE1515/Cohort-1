e6cb26a163f0d585e34e220504909beddcb291e1
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2025-02-01 21:59:26 +0530

    starting of js

---
+++ Javasript/hello.js
@@ -0,0 +1,22 @@
+
+function printchai(){
+    console.log('hello chai')
+}
+
+
+function bringBrush(numberOfBrush){
+console.log(`Hanji , le aaya brush ${numberOfBrush}`);
+
+}
+
+bringBrush(14);
+printchai();
+
+
+function addTwo(ekNum, doNum){
+    return ekNum + doNum
+}
+
+addTwo(1,1)
+console.log(addTwo(2,1));
+
