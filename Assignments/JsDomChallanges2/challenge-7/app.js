5fd00a1cb24084726f540b69787be46cb16dba92
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2025-03-06 15:25:09 +0530

    challange 7

--- Assignments/JsDomChallanges2/challenge-7/app.js
+++ Assignments/JsDomChallanges2/challenge-7/app.js
@@ -1,3 +1,16 @@
 /**
  * Write your challenge solution here
  */
+
+
+const button = document.querySelectorAll(".accordion-button")
+
+button.forEach( (data) => {
+   data.addEventListener("click", () => {
+      document.querySelectorAll(".accordion-item").forEach((e) => {
+         e.classList.remove("active")
+      })
+      data.parentElement.classList.toggle("active")
+   })
+})
+
