2594f66da982d3ffc5f41a6372431face1d543d9
KUNALSHINDE1515 <kunalshinde7956@gmail.com>
2025-03-14 16:12:03 +0530

    Assingment 9

--- Assignments/JsDomChallanges2/challenge-9/app.js
+++ Assignments/JsDomChallanges2/challenge-9/app.js
@@ -1,3 +1,14 @@
 /**
  * Write your challenge solution here
  */
+
+const toggleBtn = document.querySelector(".toggle-btn")
+const panelBtn = document.querySelector(".panel")
+const closeBtn = document.querySelector(".close-btn")
+toggleBtn.addEventListener("click", () => {
+    panelBtn.classList.toggle("active")
+})
+
+closeBtn.addEventListener("click", () => {
+    panelBtn.classList.remove("active")
+})
